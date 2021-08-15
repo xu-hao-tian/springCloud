9a6c8efbfd91f715087de2eb64d7782372a221b6
xuhaotian <ithation@163.com>
2021-08-15 09:19:50 +0800

    Gateway 新一代网关

--- cloud-gateway-gateway-9527/src/main/java/com/springcloud/filter/MyLogGateWayFilter.java
+++ cloud-gateway-gateway-9527/src/main/java/com/springcloud/filter/MyLogGateWayFilter.java
@@ -19,7 +19,7 @@ public class MyLogGateWayFilter implements GlobalFilter,Ordered {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 
         log.info("*********come in MyLogGateWayFilter: "+new Date());
-        String uname = exchange.getRequest().getQueryParams().getFirst("username");
+        String username = exchange.getRequest().getQueryParams().getFirst("username");
         if(StringUtils.isEmpty(username)){
             log.info("*****用户名为Null 非法用户,(┬＿┬)");
             exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);//给人家一个回应
